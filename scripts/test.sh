f8b9db7fc2fa4a5436c590e07a427b562ca9e345
Bernhard Haubold <haubold@evolbio.mpg.de>
2021-12-13 18:18:39 +0100

    Added fast lookup to annotate

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,5 @@
-../annotate/annotate -s ../data/hsRefGene.txt ../data/i1.txt | awk '{print $4}' | tr ',' '\n' > ../data/obsSym.txt
+../annotate/annotate -s -c ../data/hsRefGene.txt ../data/i1.txt > ../data/tmpSym.txt
 ../shuffle/shuffle -n 10000 ../data/hsTmpl.txt ../data/i1.txt |
-    ../annotate/annotate -s ../data/hsRefGene.txt |
-    awk '/^#/&&NR>1{print ""}!/^#/{print $4}' |
-    tr ',' '\n' |
-    ./ego -i ../data/Homo_sapiens.gene_info -g ../data/gene2go ../data/obsSym.txt
+    ../annotate/annotate -f -s -c ../data/hsRefGene.txt |
+    ../ego/ego -i ../data/Homo_sapiens.gene_info -g ../data/gene2go ../data/tmpSym.txt
+rm ../data/tmpSym.txt
